b324a3a6a50fd7b3d5b54c9e7214a43a6e7f43f0
tainamamederibeiro@gmail.com <tainamamederibeiro@gmail.com>
2023-09-10 14:44:45 +0100

    new code

--- code1.py
+++ code1.py
@@ -11,3 +11,10 @@ for i in range(x+1):
             l.append([i,j,k])
 print(l)
 
+########################################################
+#Another way to write the same code
+
+
+lista = [(i,j,k) for i in range(x+1) for j in range(y+1) for k in range(z+1)]
+print([(i,j,k) for i,j,k in lista if i+j+k !=n])
+
